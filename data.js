913aeda2011403684c43d1115e7fd09833f076ff
owuMode <owucart@gmail.com>
2026-09-22 17:08:34 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const PRODUCTS_DATA = [
+
+    {
+        id: 1,
+        name: "Royal Envoy 3",
+        category: "modes",
+        description: "My game description.",
+        version: "v1.0.0",
+        size: "38 MB",
+        platform: "Windows",
+        fileName: "RoyalEnvoy3_ModMenu.exe",
+        fileId: "1EvvlLFqoC4COK5wCFKLJ7YzUcYb098QN",
+        image: "https://alawarland.com/upload/information_system_1/1/0/5/item_1059/information_items_1059.jpg",
+        badge: "New",
+        link: "https://www.myrealgames.com/download-free-games/royal-envoy-3/"
+    }
+];
