2b53b62c224765a27a389df3ae856af256bec632
owuMode <owucart@gmail.com>
2026-09-22 15:06:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -353,7 +353,7 @@ function closeGameDetails() {
 
 
 /* =========================================================
-   DOWNLOAD GAME
+   DOWNLOAD MODE
    ========================================================= */
 
 function downloadGame(gameId) {
