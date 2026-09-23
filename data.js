7c73550a7c02d99369f9e3c37f4efad705a4763f
owuMode <owucart@gmail.com>
2026-09-23 14:21:58 +0530

    Update data.js

--- data.js
+++ data.js
@@ -5,13 +5,29 @@ const PRODUCTS_DATA = [
         name: "Royal Envoy 3",
         category: "modes",
         description: "How to use : \n1. Download Original Game \n2. Download this Mode \n3. First run game then open Mode \n4. Enjoy",
-        version: "2.1.0",
-        size: "38 MB",
         platform: "Windows",
-        fileName: "RoyalEnvoy3_ModMenu.exe",
-        fileId: "1EvvlLFqoC4COK5wCFKLJ7YzUcYb098QN",
         image: "https://alawarland.com/upload/information_system_1/1/0/5/item_1059/information_items_1059.jpg",
         badge: "New",
-        link: "https://www.myrealgames.com/download-free-games/royal-envoy-3/"
+        link: "https://www.myrealgames.com/download-free-games/royal-envoy-3/",
+
+        versions: [
+            {
+                version: "v1.0.0",
+                size: "38 MB",
+                fileName: "RoyalEnvoy3_ModMenu.exe",
+                fileId: "1EvvlLFqoC4COK5wCFKLJ7YzUcYb098QN",
+                date: "2026-09-01",
+                isLatest: false
+            },
+            {
+                version: "v2.0.0",
+                size: "40 MB",
+                fileName: "RoyalEnvoy3_ModMenu2.exe",
+                fileId: "1w4D4_YkPBLW8j2DEo4u9BFJQ6A-Dnpsv",
+                date: "2026-09-23",
+                isLatest: true
+            }
+        ]
     }
+
 ];
